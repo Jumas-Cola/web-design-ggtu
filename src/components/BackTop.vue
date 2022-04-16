32fefbfc3406605151cbf8c648ab6986709afd42
Jumas Cola <kbbyfl91@gmail.com>
2022-04-17 01:36:51 +0300

    add go_up button

---
+++ src/components/BackTop.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="display" @click="goUp" class="go-up p-2 pb-3">
+    <img src="/img/go_up.png" alt="Up" />
+  </div>
+</template>
+
+<style scoped>
+.go-up {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  background: #3d67b0;
+  border-radius: 50%;
+  cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      display: false,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll, true);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll, true);
+  },
+  methods: {
+    goUp() {
+      window.scrollTo(0, 0);
+    },
+    onScroll(e) {
+      if (window.pageYOffset > 1000) {
+        this.display = true;
+      } else {
+        this.display = false;
+      }
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import Slider from "./components/Slider.vue";
 import Catalog from "./components/Catalog.vue";
 import Consults from "./components/Consults.vue";
 import NewsForm from "./components/NewsForm.vue";
+import BackTop from "./components/BackTop.vue";
 
 createApp(Slider).mount("#slider");
 createApp(Catalog).mount("#catalog");
 createApp(Consults).mount("#consults");
 createApp(NewsForm).mount("#news-form");
+createApp(BackTop).mount("#go-up");
